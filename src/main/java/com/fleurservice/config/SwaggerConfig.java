bcad0fe421245b26674d7ab2d42058770b10522e
adnane.msl <msillaneadnane05@gmail.com>
2026-03-29 03:31:19 +0100

    exception

---
+++ src/main/java/com/fleurservice/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.fleurservice.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Fleur Service API")
+                        .version("1.0")
+                        .description("Microservice de gestion de la boutique de fleurs"));
+    }
+}
